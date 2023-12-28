68719c59abb53b22ff4ff59276596f4da685b7c6
deepak798 <54618348+deepak798@users.noreply.github.com>
2023-12-29 01:07:14 +0530

    Update script.js
    Added comments for better understanding of code

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// importing values from html to js 
 let timerDisplay = document.querySelector('.timerDisplay');
 let stopBtn = document.getElementById('stopBtn');
 let startBtn = document.getElementById('startBtn');
-let resetBtn = document.getElementById('clearBtn');
+let clearBtn = document.getElementById('clearBtn');
 
+// Assigning hours ,min and msec value as 0 
 let msec = 0;
 let secs = 0;
 let mins = 0;
 
 let timerId = null;
 
+//code for Start button
 startBtn.addEventListener('click', function(){
     if(timerId !== null){
         clearInterval(timerId);
@@ -16,16 +19,21 @@ startBtn.addEventListener('click', function(){
     timerId = setInterval(startTimer, 10);
 });
 
+//code for Stop button
 stopBtn.addEventListener('click', function(){
     clearInterval(timerId);
 });
 
+//code for clear button
 clearBtn.addEventListener('click', function(){
     clearInterval(timerId);
     timerDisplay.innerHTML = `00 : 00 : 00`;
     msec = secs = mins = 0;
 });
 
+/* timer function when msec become 100 then it will make it 0 and sec++ to start next iteration
+      same for sec value , when sec is equal to 60 then ,it will start second value with 0 and min++
+       */
 function startTimer(){
     msec++;
     if(msec == 100){
@@ -37,11 +45,12 @@ mins++;
         } 
     }
 
+    // if msec value is 7 then it will make 07 , same for sec and min
     let msecString = msec < 10 ? `0${msec}` : msec;
     let secsString = secs < 10 ? `0${secs}` : secs;
     let minsString = mins < 10 ? `0${mins}` : mins;
     
-
+    // Display timer in HTML  
     timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
 
 }
